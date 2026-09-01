f98336549ac6a1c4e87ef3f3063459d550fef70b
impetus <jonathan.oyola91@gmail.com>
2026-09-01 09:03:04 -0500

    fix: corrige inicializacion ASGI para chat websocket

--- config/asgi.py
+++ config/asgi.py
@@ -3,19 +3,27 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 from django.core.asgi import get_asgi_application
+
+# IMPORTANTE:
+# Primero Django debe cargar todas las aplicaciones y modelos.
+django_asgi_app = get_asgi_application()
+
+# Solo después de inicializar Django importamos Channels
+# y las rutas que terminan importando consumers/models.
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import internal_chat.routing
 
-django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
 
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            internal_chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                internal_chat.routing.websocket_urlpatterns
+            )
+        ),
+    }
+)
